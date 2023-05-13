2fdb2d0d0de9e4b0cc98a3a2c72931d3d8b47042
Alex <alex@letris.ru>
2023-05-14 00:16:34 +0800

    ord legacy format (without OP_PUSHDATA1) and new format with OP_PUSHDATA1 can be parsed now.

--- modules/ords.js
+++ modules/ords.js
@@ -1,19 +1,25 @@
 const { BitcoinStreamModule } = require("../bitcoinstreammodule.js");
+const { Uint64BE } = require("int64-buffer");
 
 function haveOrds(scriptHex) {
     return scriptHex.indexOf("0063036f72640101") != -1;
 }
 
 function getOrd(scriptHex) {
+    console.log(scriptHex);
     let buff = Buffer.from(scriptHex, 'hex');
     let startOffset = 0;
     let contentTypeStart = 0;
     let contentTypeEnd = 0;
     let contentStart = 0;
     let contentEnd = 0;
+    let pushContentByte = 0;
+    let offset = 1;
+    let contentLengthOffset = 1;
 
     let content = '';
     let contentType = '';
+    let contentLength = 1;
     let ords = {};
 
     for (let i = 0; i < buff.length - 1; i++) {
@@ -22,12 +28,43 @@ function getOrd(scriptHex) {
             contentTypeStart = i + 8;
             contentTypeEnd = contentTypeStart + buff[contentTypeStart];
             contentType = buff.subarray(contentTypeStart + 1, contentTypeEnd + 1);
+            pushContentByte = contentTypeEnd + 2;//+0x00
 
-            contentStart = contentTypeEnd + 2;
-            contentEnd = contentStart + buff[contentStart] + 1;
-            content = buff.subarray(contentStart + 1, contentEnd);
+            if (buff[pushContentByte] == 0x4c)//OP_PUSHDATA1
+                offset = 1;
+            else if (buff[pushContentByte] == 0x4d) {//OP_PUSHDATA2
+                offset = 2;
+            } else if (buff[pushContentByte] == 0x4e) {//OP_PUSHDATA4
+                offset = 4;
+            } else { //its a var_int
+                contentLengthOffset = 0;
+
+                if (buff[pushContentByte] == 253) {
+                    //uint16
+                    offset = 2;
+                } else if (buff[pushContentByte] == 254) {
+                    //uint32
+                    offset = 4
+                } else if (buff[pushContentByte] == 255) {
+                    //uint64
+                    offset = 8;
+                } else {
+                    //uint8
+                    offset = 1;
+                }
+            }
+
+
+            contentLength = buff.subarray(pushContentByte + contentLengthOffset, pushContentByte + contentLengthOffset + offset).readUIntBE(0, offset)
+            contentStart = pushContentByte + offset + contentLengthOffset;
+
+            contentEnd = contentStart + contentLength;
+            content = buff.subarray(contentStart, contentEnd);
+
 
             ords = { contentType: Buffer.from(contentType).toString(), content: Buffer.from(content).toString() };
+
+            break;
         }
     }
 
